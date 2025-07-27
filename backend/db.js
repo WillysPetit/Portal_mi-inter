d399e7946b5a89fcdd718610829edbd8ea0801ff
Willys Petit <w.petit@urbe.edu.ve>
2025-07-27 16:34:48 -0400

    intento 1

--- backend/db.js
+++ backend/db.js
@@ -1,10 +1,11 @@
 import mysql from "mysql2";
 
 const connection = mysql.createConnection({
-  host: "localhost",
-  user: "root",
-  password: "",
-  database: "portal mi-inter",
+  host: process.env.DB_HOST || "localhost",
+  user: process.env.DB_USER || "root",
+  password: process.env.DB_PASSWORD || "",
+  database: process.env.DB_NAME || "Portal_mi-inter",
+  port: process.env.DB_PORT ? parseInt(process.env.DB_PORT) : 3306,
 });
 
 connection.connect((err) => {
